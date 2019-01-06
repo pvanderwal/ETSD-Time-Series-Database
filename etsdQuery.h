2ab814255e6338e07e4a6346e01dca1599eccd99
pvanderwal <45677879+pvanderwal@users.noreply.github.com>
2019-01-05 21:15:46 -0700

    Update etsdQuery.h

--- etsdQuery.h
+++ etsdQuery.h
@@ -14,7 +14,7 @@ Copyright 2018 Peter VanDerWal
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 *********************************************************************************/
-        
+
 #ifndef __etsdquery_h__
 #define __etsdquery_h__
 
@@ -23,20 +23,23 @@ extern "C" {
 #endif
 
 
-// calculates total seconds when presented with time as ##S or ##M or ##H, or ##Y.
-int parseT(char *val);
+// calculates total seconds when presented with time as +/- ########S, ########M, #######H, #####d, or ##Y representing seconds, minutes, hours, days, or years
+int32_t parseT(char *val);
 
-int etsdParseTime(char *dt);
+// converts input time specification to epoch time
+uint32_t etsdParseTime(char *dt);
 
-// converts an etsd time string into Epoch time
-int etsdTime(char *dt);
+// creates an RRD database that matches ETSD as specified in arguments.  rraV specifies the RRA to build
+void buildRRD(char *fName, uint8_t channels, uint8_t chanMap[], uint32_t totalTime, uint32_t datapoints, int rraC, char *rraV[]);
 
-int buildRRD(char *fName, uint8_t channels, uint8_t chanMap[], int totalTime, int datapoints, int rraC, char *rraV[]);
+int32_t recoverRRD(int argc, char *argv[], char *rrdFName, uint8_t overwrite);
 
-int recoverRRD(int argc, char *argv[], char *rrdFName);
+// returns the channel number of chanName, or 255 if not found
+uint8_t etsdChanNum(char *chanName);
 
-uint32_t etsdQ(char *cmd, uint8_t chan, uint32_t start, uint32_t stop, uint32_t val);
+int64_t etsdAMT(char *cmd, uint8_t chan, uint32_t start, uint32_t stop);
 
+uint32_t etsdQ(int argc, char *argv);
 
 #ifdef __cplusplus
 }
