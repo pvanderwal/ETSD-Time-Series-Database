7ddff9b64cc755d6cb1664b7afe01e420e29b831
pvanderwal <45677879+pvanderwal@users.noreply.github.com>
2019-01-05 21:13:48 -0700

    Update errorlog.h

--- errorlog.h
+++ errorlog.h
@@ -24,32 +24,33 @@ extern "C" {
 #endif
 
 // Error Codes
-// Error Codes 1-16 are Info, 32-512 are warnings are 'Notices' not displayed if LogLvl is less than 2
-// error levels between 1024 and 4096 are 'Warnings', generally operator error, 2048 and up are hard errors
-#define E_DATA            1     // Data is invalid
+// Error Codes 1-16 are Info, 32-512 are 'Notices' 1-512 not displayed if LogLvl is less than 2
+// error levels 1024 to 16384 are 'Warnings', generally operator error, 32768 and up are hard errors
+#define E_DATA            1     // Interval data is invalid
 #define E_EOF             2     // End of file reached
 #define E_BEFORE          4     // Target time is BEFORE ETSD starts
 #define E_AFTER           8     // Target time is AFTER ETSD ends
 #define E_NOT_FOUND      16     // Target time is not in ETSD
-#define E_STARTSTOP     32     // Invalid start/stop time
+#define E_STARTSTOP      32     // Invalid start/stop time
 #define E_DATE           64     // Invalid date format
 #define E_ARG           128     // Invalid arguement
 //#define E_UNK           256  
 //#define E_UNK           512  
 #define E_NOT_ETSD     1024     // File header block(0) not from ETSD file
 #define E_NOT_TTY      2048     // Device specified is not a TTY device
-//#define E_UNK          4096
-#define E_TTY_STAT     8192     // Can't get status of tty device
-#define E_SHM         16384     // Can't open/create shared memory object
-#define E_CANT_WRITE  32768     // Can't open file for writing
-#define E_CANT_READ   65536     // Can't open file for reading
-#define E_SEEK       131072     // Error seeking sector
-#define E_CHECKSUM   262144     // Check Sum error on ECM packet
-#define E_TIMEOUT    524288     // Timed out waiting on data from ECM-1240
-//#define E_UN        1048576
-//#define E_UN        2097152
-//#define E_UNK       4194304
-//#define E_UNK       8388608
+#define E_RRD          4096     // Excessive RRD errors, aborting
+//#define E_UN           8192
+//#define E_UNK         16384   
+#define E_TTY_STAT    32768     // Can't get status of tty device
+#define E_SHM         65536     // Can't open/create shared memory object
+#define E_CANT_WRITE 131072     // Can't open file for writing
+#define E_CANT_READ  262144     // Can't open file for reading
+#define E_SEEK       524288     // Error seeking sector
+#define E_CHECKSUM  1048576     // Check Sum error on ECM packet
+#define E_TIMEOUT   2097152     // Timed out waiting on data from ECM-1240
+#define E_DELETE    4194304     // Can't delete/overwrite file
+
+#define E_NO_ETSD   8388608     // ETSD database not initialized
 //#define E_UNK      16777216
 //#define E_UNK      33554432
 //#define E_UNK      67108864   // Unknown Error
